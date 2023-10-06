83c85e57e37234a576a225ba25e88ff8a7056ead
Anish-Branch <95915369+Anish-Branch@users.noreply.github.com>
2023-10-06 11:57:28 +0100

    Initial Commit

---
+++ bumonsterapp/bumonsterappApp.swift
@@ -0,0 +1,17 @@
+//
+//  bumonsterappApp.swift
+//  bumonsterapp
+//
+//  Created by Anish Somani on 06/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct bumonsterappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
